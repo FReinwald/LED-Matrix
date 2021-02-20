6ba01534ba2649276844597b564b8d6fd0e93d2b
FReinwald <reinwald.fabian@web.de>
2021-02-20 16:38:42 +0100

    added specs file and started working on menu

--- src/main.cpp
+++ src/main.cpp
@@ -7,94 +7,132 @@
 //*******************************************************************
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include "specs.h"
 /*#ifdef __AVR__
   #include <avr/power.h>
 #endif*/
+int R =0;
 //*******************************************************************
-//PIN am Arduino, an dem die LEDs angeschlossen sind
-#define PIN 6
-//Anzahl der LEDs
-#define NUMPIXELS 12
-//Pause zwischen Durchlauf
-#define DELAYVAL 1000
-//Input PIN X-Achse
-#define X_PIN 2
-//Input PIN Y-Achse
-#define Y_PIN 4
-//Input Push PIN
-#define Z_PIN 7
-
-
-
-Adafruit_NeoPixel pixels(NUMPIXELS,PIN,NEO_GRB+NEO_KHZ800);
+Adafruit_NeoPixel pixels(NUMPIXELS,C_PIN,NEO_GRB+NEO_KHZ800);
 //*******************************************************************
+void AllLED()
+{
+  delay(29);
+}
+
+void SingleLED()
+{
+  delay(29);
+}
+
+void ColorControl(int PIN)
+{
+  delay(29);
+}
+
+void Program(int MenuState)
+{
+  switch (MenuState)
+  {
+  case 1:
+    AllLED();
+    break;
+  case 2:
+    SingleLED();
+    break;
+  default:
+   // MainMenu();
+    break;
+  }
+}
+
+void MainMenu()
+{
+  pixels.begin();
+  int MenuState=1;
+  bool lastR = R_PIN_Input;
+  bool lastB = B_PIN_Input;
+  //MainMenu Interface
+  for(int i=0;i<NUMPIXELS;i++)
+  {
+    pixels.setPixelColor(i,pixels.Color(0,0,50));
+  }
+  pixels.show();
 
 
-
+  do
+  {
+    if(R_PIN_Input)
+    {
+      if(MenuState==1)
+      {
+        MenuState=MENUOPTIONS;
+      }
+      else
+      {
+        MenuState--;
+      }
+      //Move Cursor
+      pixels.begin();
+      pixels.setPixelColor(MenuState,pixels.Color(10,0,10));
+      pixels.show();
+    }
+    if(!B_PIN_Input && !lastB)
+    {
+      if(MenuState==MENUOPTIONS)
+      {
+        MenuState=1;
+      }
+      else
+      {
+        MenuState++;
+      }
+      //Move Cursor
+      pixels.setPixelColor(MenuState,pixels.Color(0,0,10));
+      pixels.show();
+    }
+    if(!Util_PIN_Input)
+    {
+      Program(MenuState);
+    }
+    lastR = R_PIN_Input;
+    lastB = B_PIN_Input;
+  } while (Util_PIN_Input);
+}
+//*******************************************************************
 void setup() {
 // put your setup code here, to run once:
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
   clock_prescale_set(clock_div_1);
 #endif
 
-  pixels.begin();   //Initialize NeoPixel strip object
-  pixels.clear();
-
-  //Initialize Joystick Pins
-  pinMode(X_PIN, INPUT_PULLUP);
-  pinMode(Y_PIN, INPUT_PULLUP);
-  pinMode(Z_PIN, INPUT_PULLUP);
-
-    pixels.clear();
-
+  //Initialize NeoPixel strip object
+  pixels.begin();
   for(int i=0;i<NUMPIXELS;i++)
   {
-    pixels.setPixelColor(i,pixels.Color(0,1,0));
+    pixels.setPixelColor(i,pixels.Color(40,0,50));
   }
   pixels.show();  
+  //pixels.clear();
+
+  //Initialize Joystick Pins
+  pinMode(R_PIN, INPUT_PULLUP);
+  pinMode(G_PIN, INPUT_PULLUP);
+  pinMode(B_PIN, INPUT_PULLUP);
+  pinMode(C_PIN, INPUT_PULLUP);
+
+
+  //Start Main-Menu
+ 
+  MainMenu(); 
 }
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-
-
-  int X_PIN_Input = !digitalRead(X_PIN);
-  int Y_PIN_Input = !digitalRead(Y_PIN);
-  int Z_PIN_Input = !digitalRead(Z_PIN);
-  int static index = 0;
-
-if(X_PIN_Input)
- {
-   pixels.setPixelColor(index,pixels.Color(10,10,10));
-   pixels.show();
-   index++;
- }
-
- if(Y_PIN_Input)
- {
-   for(int i=0;i<NUMPIXELS;i++)
-   {
-    pixels.setPixelColor(i,pixels.Color(0,0,1));
-  }
-   pixels.show();
-   index++;
- }
-
-  if(Z_PIN_Input == true)
+pixels.begin();
+  for(int i=0;i<NUMPIXELS;i++)
   {
-    pixels.setPixelColor(index,pixels.Color(100,50,150));
-    pixels.show();
-    index++;
+    pixels.setPixelColor(i,pixels.Color(40,100,50));
   }
- 
-
-
-  delay(50);
-
-  if(index>=NUMPIXELS)
-  {
-    index=0;
-  }
+  pixels.show(); 
 }
---
+++ src/specs.h
@@ -0,0 +1,28 @@
+/**
+  Specs for the LED Matrix and the connected Arduino UNO
+  Author: Fabian Reinwald
+  Date: 20.02.2021
+  Version: 1.0
+*/
+
+//PIN controlling the LEDs
+#define C_PIN 6
+//Number of LEDs
+#define NUMPIXELS 12
+//Delay between loops
+#define DELAYVAL 1000
+//Input Next/Blue
+#define B_PIN 2
+//Input Green
+#define G_PIN 4
+//Input Previous/Red
+#define R_PIN 7
+//Input PIN On/Off/Select
+#define UTIL_PIN 8
+//Number of Menu Options
+#define MENUOPTIONS 3
+
+int R_PIN_Input = !digitalRead(R_PIN);
+int G_PIN_Input = !digitalRead(G_PIN);
+int B_PIN_Input = !digitalRead(B_PIN);
+int Util_PIN_Input = !digitalRead(UTIL_PIN);
